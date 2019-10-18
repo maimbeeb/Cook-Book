7b351db4b69ed872b85a0d1d8c8f74a5d47c6a43
maimbeeb <>
2019-10-18 23:07:56 +0530

    Recipe static template implemented with Python Flask

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+@app.route('/recipe/')
+def recipe():
+    return render_template('recipe.html')    
 if __name__ == '__main__':
     app.run(debug=True)
